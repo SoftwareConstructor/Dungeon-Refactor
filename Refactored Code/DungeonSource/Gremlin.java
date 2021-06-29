bc38a7c53d03557f2c97c2e83b3b0c2a45091406
AsmaSalsabilaYunos <asmasalsabilamy@gmail.com>
2021-06-29 18:27:48 +0800

    long param refactor

--- Refactored Code/DungeonSource/Gremlin.java
+++ Refactored Code/DungeonSource/Gremlin.java
@@ -14,7 +14,8 @@ public class Gremlin extends Monster
 
     public Gremlin()
 	{
-		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, 20, 40);
+		super("Gnarltooth the Gremlin", 70, 5, .8, 15, 30);
+		monsterHeal(.4, 20, 40);
 
     }//end constructor
 
--- Refactored Code/DungeonSource/Skeleton.java
+++ Refactored Code/DungeonSource/Skeleton.java
@@ -14,10 +14,12 @@ public class Skeleton extends Monster
 
     public Skeleton()
 	{
-		super("Sargath the Skeleton", 100, 3, .8, .3, 30, 50, 30, 50);
+		super("Sargath the Skeleton", 100, 3, .8, 30, 50);
+		monsterHeal(.3, 30, 50);
 
     }//end constructor
 
+	@Override
 	public void attack(DungeonCharacter opponent)
 	{
 		System.out.println(name + " slices his rusty blade at " +
